c1ab9ab12faef23c7d13cb95a8cadbc897be2b5c
ionanghelina <mariananghelina12@yahoo.com>
2021-06-04 00:08:44 +0300

    solved film to filme page bug

--- index.js
+++ index.js
@@ -103,6 +103,29 @@ app.get("/filme",function(req, res){
     
 });
 
+app.get("/film/filme",function(req, res){
+    // console.log("Pagina de filme");
+ 
+     //console.log("Url:",req.url);
+     //console.log("Query:", req.query.tip);
+     // conditie_booleana? val_true : val_false
+     let conditie= req.query.tip ?  " and tip_produs='"+req.query.tip+"'" : "";//daca am parametrul tip in cale (tip=cofetarie, de exemplu) adaug conditia pentru a selecta doar produsele de acel tip
+    // console.log("select * from filme where 1=1"+conditie);
+     client.query("select * from filme where 1=1"+conditie, function(err,rez){
+         //console.log(err, rez);
+         //console.log(rez.rows);
+         client.query("select unnest(enum_range( null::categ_film)) as categ", function(err,rezCateg){//selectez toate valorile posibile din enum-ul categ_prajitura
+ 
+            // console.log(rezCateg);
+             res.render("pagini/filme", {produse:rez.rows, categorii:rezCateg.rows});//obiectul {a:10,b:20} poarta numele locals in ejs  (locals["a"] sau locals.a)
+             });
+         
+        
+     });
+ 
+     
+ });
+
 app.get("/film/:id",function(req, res){
   
   //  console.log(req.params);
